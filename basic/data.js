6ece3cefb4cf16c0698b4fdb0edba71bff4ce717
SangeetaRajput1540 <sangeeta.rajput1540@gmail.com>
2025-05-17 03:02:48 +0000

    best chapter

---
+++ basic/data.js
@@ -0,0 +1,18 @@
+let score = true
+
+let valueInNumber = Number(score)
+//console.log(typeof score)
+
+//console.log(valueInNumber)
+
+let foologIn = ""
+let BoolfoologIn = Boolean(foologIn)
+
+//console.log( BoolfoologIn)
+//console.log(typeof foologIn)
+
+let stringNumber = "sita"
+let StringStringNumber  = String(stringNumber)
+
+console.log(typeof stringNumber)
+console.log(StringStringNumber)
---
+++ basic/variable.js
@@ -0,0 +1,6 @@
+const userName = "sangeeta"
+let age = 22
+var string = null
+let accoutId = true
+
+console.table([typeof userName, age, string, accoutId])
