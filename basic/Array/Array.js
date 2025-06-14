11cb04e70e158f2667450ab77e67b0f21a584054
SangeetaRajput1540 <sangeeta.rajput1540@gmail.com>
2025-06-14 03:35:58 +0000

    best

--- basic/Array/Array.js
+++ basic/Array/Array.js
@@ -1,27 +1,45 @@
-let myArr = [0, 1, 2, 3, 4, 5]
+//let myArr = [0, 1, 2, 3, 4, 5]
 //console.log(myArr[2])
 
 const myArr2 = ['sangeeta', 'nishant','kapil']
 const bestArr = new Array (1, 2, 3)
 
-myArr.push(7)
+//myArr.push(7)
 //console.log(myArr)
 
-myArr.pop()
+//myArr.pop()
 //console.log(myArr)
 
-myArr.unshift(0)
+//myArr.unshift(0)
 //console.log(myArr)
 
-myArr.shift()
+//myArr.shift()
 //console.log(myArr)
 
 
 ///************yaha ek Q KA KAAM Krte hai ******* *//////////////////////////
 
-console.log(myArr.includes(9))
-console.log(myArr.indexOf(10))
+//console.log(myArr.includes(9))
+//console.log(myArr.indexOf(10))
 
-const sreachArr =  myArr.join()
+//const sreachArr =  myArr.join()
 
-console.log(sreachArr)
+//console.log(sreachArr)
+
+const myArr =  [  {
+   name : "sangeeta"
+}]
+
+myArr[0].age =22;
+myArr.push({name : "nishant"})
+
+//console.log(myArr)
+
+
+myArr.map(person => {
+ 
+    console.log(person.name);
+  
+});
+
+console.log(myArr)
--- basic/String.js
+++ basic/String.js
@@ -8,10 +8,35 @@ let score = 1.5
 
 //console.log(`the best in the world ${gameName} the game score ${score}`)
 
-const string4 = new String("Aobjectbvfgj");
+const string4 = new String("Aobjectbvfgj ");
 
 //console.log(string4[2])
 
-console.log(string4.split())
-console.log(string4.charCodeAt(0))
-console.log(string4.indexOf("b"))
+//console.log(string4.split())
+//console.log(string4.charCodeAt(1))
+//console.log(string4.indexOf("j"))
+
+//console.log(string4.at(2))
+//console.log(string4.charAt(2))
+
+//console.log(string4.lastIndexOf("j"))
+
+//console.log(string4.includes("n"))
+
+//console.log(string4.startsWith("n"))
+
+//console.log(string4.endsWith("n"))
+
+//console.log(string4.trim())
+
+//console.log(string4.slice(0 ,5))
+
+const user = "I AM best student , sangeeta"
+
+console.log(user.replace("student", "cute girl"))
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+let Str = "welcome"
+
+console.log(Str.charAt(-1))
